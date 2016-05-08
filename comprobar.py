e24c94c51aa57d381bfac543bdf05e2db39b9298
victoralonsopacha <victoralonsopacha@outlook.es>
2016-05-07 20:46:37 -0500

    area 1.1
    función tangente en el calculo del área del polígono

--- comprobar.py
+++ comprobar.py
@@ -1,3 +1,13 @@
-import math
-a= math.tan(36)
-print (a)
+def apotema(n):
+	alfa= 360/5
+    den1= 2 * math.tan (36) * (180/math.pi)
+    den2= den1 * (180/math.pi)
+    apotema=n/den2
+    print(apotema)
+
+def area_pentagono(n, m):
+    area=(5*n*m)/2
+    print(area)
+
+a=apotema()
+area_pentagono(a, 3.6)
