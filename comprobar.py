fe67bc7a10047272c64840b011df70df7e3c155e
victoralonsopacha <victoralonsopacha@outlook.es>
2016-05-07 21:16:41 -0500

    version1.4
    nueva version de la comprobacion de la funcion tangente

--- comprobar.py
+++ comprobar.py
@@ -1,13 +1,16 @@
+import math
+
 def apotema(n):
-	alfa= 360/5
-    den1= 2 * math.tan (36) * (180/math.pi)
-    den2= den1 * (180/math.pi)
-    apotema=n/den2
-    print(apotema)
+    alfa = 360/5
+    den1= 2 * math.tan (36)
+    den2 = den1 * (180/ math.pi)
+    ap = n/den2
+    return ap
+    
 
 def area_pentagono(n, m):
     area=(5*n*m)/2
     print(area)
 
-a=apotema()
+a=apotema(5)
 area_pentagono(a, 3.6)
